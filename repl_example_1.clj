0fbaeb550bf541536f15649139d5e0555e2f3eee
Alex Coyle <abcoyle@gmail.com>
2016-04-01 11:59:50 +0100

    Modified examples to use Sparkling instead of Flambo

--- repl_example_1.clj
+++ repl_example_1.clj
@@ -1,25 +1,25 @@
 (use 'clojure.repl)
 
-(use 'alzatech.data-exp.flambo.context)
+(use 'alzatech.data-exp.sparkling.context)
 
 ; ..ok, let's make a spark context
 
 (def ctx (spark-context-for-app-named "friday-afternoon-examples"))
 
-(require '[flambo.api :as f])
+(require '[sparkling.core :as sc])
 
-(def numbers (f/parallelize ctx [1 2 3 4 5])) ; parallelize an existing collection
+(def numbers (sc/parallelize ctx [1 2 3 4 5])) ; parallelize an existing collection
 
 numbers ; oops, it's lazy
 
-(f/take numbers 3) ; action (non-lazy)
+(sc/take 3 numbers) ; action (non-lazy)
 
-(f/defsparkfn square-of [number] (* number number)) ; define a named, serializable spark function
+(defn square-of [number] (* number number)) ; define a named, serializable spark function
 
-(f/map numbers square-of) ; oops, transformation (lazy)
+(sc/map square-of numbers) ; oops, transformation (lazy)
 
 ; ..ok, let's look at a sample by wrapping with an action to force computation
 
-(f/take (f/map numbers square-of) 3) ; a transform with subsequent action
+(sc/take 3 (sc/map square-of numbers)) ; a transform with subsequent action
 
-(f/collect (f/map numbers square-of)) ; a transform with subsequent action
+(sc/collect (sc/map square-of numbers)) ; a transform with subsequent action
--- repl_example_3.clj
+++ repl_example_3.clj
@@ -2,59 +2,59 @@
 
 (use 'clojure.repl)
 
-(use 'alzatech.data-exp.flambo.context)
+(use 'alzatech.data-exp.sparkling.context)
 
 ; make a spark context
 
 (def ctx (spark-context-for-app-named "friday-afternoon-word-freq-distributed"))
 
-(require '[flambo.api :as f])
+(require '[sparkling.core :as sc])
 
 ; create a rdd of lines
 
-(def lines (f/text-file ctx "data/quotes.txt"))
+(def lines (sc/text-file ctx "data/quotes.txt"))
 
 ; how many lines?
 
-(f/count lines) ; action (non-lazy)
+(sc/count lines) ; action (non-lazy)
 
 ; ok.. how to split a line into words?
 
 (require '[clojure.string :as s])
 
-(s/split (f/first lines) #" ") ; action (non-lazy)
+(s/split (sc/first lines) #" ") ; action (non-lazy)
 
 ; ..but we want to split *every* line into words, 
 ; flattened into a single seq, in order to process all the words sequentially..
 
-(f/flat-map lines (f/fn [line] (s/split line #" "))) ; oops, transformation (lazy)
+(sc/flat-map (fn [line] (s/split line #" ")) lines) ; oops, transformation (lazy)
 
 ; ..ok, let's look at a sample by wrapping with an action to force computation
 
-(def words (f/flat-map lines (f/fn [line] (s/split line #" "))))
+(def words (sc/flat-map (fn [line] (s/split line #" ")) lines))
 
-(f/take words 2) ; wrap with action (non-lazy)
+(sc/take 2 words) ; wrap with action (non-lazy)
 
 ; ok.. we need to go from words to word-count key-value pairs (or tuples), 
 ; so that we can reduce by key
 
-(require '[flambo.tuple :as ft])
+(require '[sparkling.destructuring :as sd])
 
-(f/map-to-pair words (f/fn [word] (ft/tuple word 1))) ; oops, transformation (lazy)
+(sc/map-to-pair (fn [word] (sd/tuple word 1)) words) ; oops, transformation (lazy)
 
 ; ..ok, let's look at a sample by wrapping with an action to force computation
 
-(def word-count-pairs (f/map-to-pair words (f/fn [word] (ft/tuple word 1))))
+(def word-count-pairs (sc/map-to-pair (fn [word] (sd/tuple word 1)) words))
 
-(f/take word-count-pairs 2) ; wrap with action (non-lazy)
+(sc/take 2 word-count-pairs) ; wrap with action (non-lazy)
 
 ; ok.. now we want to reduce word-count pairs by key, to get their frequency..
 
-(f/reduce-by-key word-count-pairs (f/fn [count1 count2] (+ count1 count2))) ; oops, transformation (lazy)
+(sc/reduce-by-key (fn [count1 count2] (+ count1 count2)) word-count-pairs) ; oops, transformation (lazy)
 
 ; ..ok, let's look at a sample by wrapping with an action to force computation
 
-(def word-counts (f/reduce-by-key word-count-pairs (f/fn [count1 count2] (+ count1 count2))))
+(def word-counts (sc/reduce-by-key (fn [count1 count2] (+ count1 count2)) word-count-pairs))
 
-(f/take word-counts 5) ; wrap with action (non-lazy)
+(sc/take 5 word-counts) ; wrap with action (non-lazy)
 
